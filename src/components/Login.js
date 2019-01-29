9fd608960eeeb250dab29dced1775b1d0c56dd99
mudithm <mudithm@gmail.com>
2019-01-29 04:57:50 -0600

    Updated Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap"
+
 
 const Login = () => (
-  <div>
-    <h1>Log In Here!</h1>
-    <RegisterButton />
-  </div>
+	<div>
+	<h1>Log in to Pondi</h1>
+	<LoginForm />
+	<RegisterButton />
+	</div>
 )
 
 const RegisterButton = () => (
@@ -14,5 +17,88 @@ const RegisterButton = () => (
         <Link to='/register'>Sign up now!</Link>
     </div>
 )
+
+
+
+class LoginForm extends React.Component {
+	state = {
+		toProfile: false,
+		email: "",
+		password: ""
+	}
+
+	nameHandler = () => {
+		// do something here
+		this.setState(() => ({
+			toProfile: true
+		}));
+	}
+
+	passHandler = (uPass) => {
+		// do something here
+			this.setState(() => ({
+			toProfile: true
+		}));	}
+
+	handleChange = event => {
+		this.setState({
+			[event.target.id]: event.target.value
+		});
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+	}
+
+	validateForm() {
+		return this.state.email.length > 0
+		&& this.state.password.length > 0;
+	}
+
+	render() {
+		if (this.state.toProfile === true) {
+					
+		}
+
+		return (
+				<form onSubmit={this.handleSubmit}>
+
+				<FormGroup controlId="email" bsSize="large">
+					<ControlLabel>Email</ControlLabel>
+					<FormControl 
+					autoFocus 
+					type="email" 
+					value={this.state.email} 
+					onChange={this.handleChange} 
+					/>
+				</FormGroup>
+
+				<FormGroup controlId="password" bsSize="large">
+            		<ControlLabel>Password</ControlLabel>
+                	<FormControl
+                	autoFocus
+                	type="password"
+                	value={this.state.password}
+                	onChange={this.handleChange}
+                	/>
+            	</FormGroup>
+
+			<Button
+                block
+                bsSize="large"
+                disabled={!this.validateForm()}
+                type="submit"
+            >Log In
+            </Button>
+
+
+				</form>
+				);
+	}
+
+
+
+}
+
 
 export default Login
