1f71a73b2223cecbab16858259335f32dc5a9aad
Ryan Lam <ryan.lam53@gmail.com>
2019-02-08 18:10:47 -0800

    Updates to input form

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import { FormControl, InputGroup, Button } from "react-bootstrap";
 
 class InputForm extends React.Component {
@@ -28,24 +27,26 @@ class InputForm extends React.Component {
     console.log(this.state.text);
     return (
       <div>
+        <h1>Prompt</h1>
         <input
           type="text"
           value={this.state.text}
           onChange={this.textInputHandler}
         />
-        <input type="file" onChange={this.fileChangedHandler} />
+        <br />
 
-        <Link to="/">
-          {" "}
-          <Button className="btn btn-primary">Submit</Button>
-        </Link>
         <div>
           {" "}
-          <img src={this.state.imagefile} height="300" width="300" />
+          <img src={this.state.imagefile} height="300" width="500" />
+          <input type="file" onChange={this.fileChangedHandler} />
           <Button className="btn btn-primary" onClick={this.uploadHandler}>
             Upload
           </Button>
         </div>
+        <Link to="/">
+          {" "}
+          <Button className="btn btn-primary">Submit</Button>
+        </Link>
       </div>
     );
   }
