49cbe1c8f94f055f37ab57c321a5450bd699a1ec
Ryan Lam <ryan.lam53@gmail.com>
2019-03-01 18:39:04 -0800

    Updated prompts page

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,5 +1,62 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FormControl, InputGroup, Button } from "react-bootstrap";
 
-const Prompt = () => <div>Hello</div>;
+class Prompt extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imagefile: null,
+      text: "",
+      prompt: "Prompt"
+    };
+  }
+
+  fileChangedHandler = event => {
+    this.setState({ imagefile: URL.createObjectURL(event.target.files[0]) });
+  };
+
+  textInputHandler = event => {
+    this.setState({ text: event.target.value });
+  };
+
+  uploadHandler = () => {
+    console.log(this.state.imagefile);
+  };
+
+  render() {
+    console.log(this.state.text);
+    return (
+      <div>
+        <div>{this.state.prompt}</div>
+
+        <textarea
+          type="text"
+          rows="20"
+          cols="80"
+          value={this.state.text}
+          onChange={this.textInputHandler}
+          placeholder="Enter text..."
+        />
+        <div>
+          {" "}
+          <img src={this.state.imagefile} height="300" width="500" />
+          <input
+            type="file"
+            accept="image/*"
+            onChange={this.fileChangedHandler}
+          />
+          <Button className="btn btn-primary" onClick={this.uploadHandler}>
+            Upload Image
+          </Button>
+        </div>
+        <Link to="/">
+          {" "}
+          <Button className="btn btn-success">Submit</Button>
+        </Link>
+      </div>
+    );
+  }
+}
 
 export default Prompt;
