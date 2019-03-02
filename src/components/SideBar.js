e7ec2def7171e9423ec33985f156c447bc12f298
Ryan Lam <ryan.lam53@gmail.com>
2019-03-01 18:35:34 -0800

    Created prompt page

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,76 +1,70 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { CSSTransitionGroup } from 'react-transition-group'; // ES6
-import { Button } from "react-bootstrap"
-import '../styles/transitions.css';
-import '../styles/sidebar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { CSSTransitionGroup } from "react-transition-group"; // ES6
+import { Button } from "react-bootstrap";
+import "../styles/transitions.css";
+import "../styles/sidebar.css";
 
-class ProfPic extends React.Component
-{
-	render()
-	{
-		return (
-
-		<div className="ProfPic">
-			<img ></img>
-
-		</div>
-
-		);
-	}
+class ProfPic extends React.Component {
+  render() {
+    return (
+      <div className="ProfPic">
+        <img />
+      </div>
+    );
+  }
 }
 
+class SideBar extends React.Component {
+  render() {
+    var addStream = this.props.addStream;
+    var addOcean = this.props.addOcean;
+    var addArchive = this.props.addArchive;
+    var addPrompt = this.props.addPrompt;
+    return (
+      <div className="SideBar">
+        <ProfPic />
 
-
-class SideBar extends React.Component
-{
-	render()
-	{
-
-		var addStream = this.props.addStream;
-		var addOcean = this.props.addOcean;
-		var addArchive = this.props.addArchive;
-		return (
-			<div className="SideBar">
-			<ProfPic />
-
-			<div className="Username">Username</div>
-			<Button
-				className = "Button"
-                block
-                bsSize="large"
-                addStream={this.props.addStream}
-                onClick={(i)=>addStream('somevar')}
-            >stream
-            </Button>
-            <Button
-            className = "Button"
-                block
-                bsSize="large"
-                addOcean={this.props.addOcean}
-                onClick={(i)=>addOcean('somevar')}
-            >ocean
-            </Button>
-            <Button
-            className = "Button"
-                block
-                bsSize="large"
-                addArchive={this.props.addArchive}
-                onClick={(i)=>addArchive('somevar')}
-            >archive
-            </Button>
-            <Button
-            className = "Button"
-                block
-                bsSize="large"
-                addArchive={this.props.addArchive}
-                onClick={(i)=>addArchive('somevar')}
-            >prompt
-            </Button>
-			</div>
-		)
-	}
+        <div className="Username">Username</div>
+        <Button
+          className="Button"
+          block
+          bsSize="large"
+          addStream={this.props.addStream}
+          onClick={i => addStream("somevar")}
+        >
+          stream
+        </Button>
+        <Button
+          className="Button"
+          block
+          bsSize="large"
+          addOcean={this.props.addOcean}
+          onClick={i => addOcean("somevar")}
+        >
+          ocean
+        </Button>
+        <Button
+          className="Button"
+          block
+          bsSize="large"
+          addArchive={this.props.addArchive}
+          onClick={i => addArchive("somevar")}
+        >
+          archive
+        </Button>
+        <Button
+          className="Button"
+          block
+          bsSize="large"
+          addArchive={this.props.addPrompt}
+          onClick={i => addPrompt("somevar")}
+        >
+          prompt
+        </Button>
+      </div>
+    );
+  }
 }
 
-
-export default SideBar
+export default SideBar;
--- src/components/StreamHolder.js
+++ src/components/StreamHolder.js
@@ -1,102 +1,161 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import { Link } from 'react-router-dom';
-import { Button, FormGroup, FormControl, ControlLabel, Alert } from "react-bootstrap";
-import { CSSTransitionGroup } from 'react-transition-group'; // ES6
-import '../styles/transitions.css';
-import '../styles/streamholder.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Alert
+} from "react-bootstrap";
+import { CSSTransitionGroup } from "react-transition-group"; // ES6
+import "../styles/transitions.css";
+import "../styles/streamholder.css";
 
-import Stream from './Stream.js';
-import Ocean from './Ocean.js';
-import Archive from './Archive.js';
-import SideBar from './SideBar.js';
+import Stream from "./Stream.js";
+import Ocean from "./Ocean.js";
+import Archive from "./Archive.js";
+import SideBar from "./SideBar.js";
+import Prompt from "./Prompt.js";
 
-
-class StreamHolder extends React.Component{
-
-
+class StreamHolder extends React.Component {
   constructor(props) {
-         
     super(props);
     this.state = {
-        stream: true,
-        archive: false,
-        ocean: false,
-        leftSide: <SideBar 
-            addStream={this.addStream.bind(this)} 
-            addArchive={this.addArchive.bind(this)}
-            addOcean={this.addOcean.bind(this)}
-            />,
-        rightSide: <Stream key={0} unmountMe={this.handleChildUnmount} />
+      stream: true,
+      archive: false,
+      ocean: false,
+      prompt: false,
+      leftSide: (
+        <SideBar
+          addStream={this.addStream.bind(this)}
+          addArchive={this.addArchive.bind(this)}
+          addOcean={this.addOcean.bind(this)}
+          addPrompt={this.addPrompt.bind(this)}
+        />
+      ),
+      rightSide: <Stream key={0} unmountMe={this.handleChildUnmount} />
     };
     this.handleAdd = this.handleAdd.bind(this);
     this.addStream = this.addStream.bind(this);
     this.addOcean = this.addStream.bind(this);
     this.addArchive = this.addStream.bind(this);
-
+    this.addPrompt = this.addPrompt.bind(this);
   }
 
-  handleAdd(i) {
-  }
+  handleAdd(i) {}
 
-  addStream(i){
-      if (! this.state.stream)
-    {
-      setTimeout(function(){
-        this.setState({rightSide: <div></div>})
-      }.bind(this), 500);
-      const right = <Stream key={0}/>;
-      setTimeout(function(){
-        this.setState({rightSide: right});
-      }.bind(this), 1000);
-      this.setState({archive: false,stream:true,ocean:false})
+  addStream(i) {
+    if (!this.state.stream) {
+      setTimeout(
+        function() {
+          this.setState({ rightSide: <div /> });
+        }.bind(this),
+        500
+      );
+      const right = <Stream key={0} />;
+      setTimeout(
+        function() {
+          this.setState({ rightSide: right });
+        }.bind(this),
+        1000
+      );
+      this.setState({
+        archive: false,
+        stream: true,
+        ocean: false,
+        prompt: false
+      });
     }
   }
 
-  addOcean (i){
-    if (! this.state.ocean)
-    {
-      setTimeout(function(){
-        this.setState({rightSide: <div></div>})
-      }.bind(this), 500);
-      const right = <Ocean key={1}/>;
-      setTimeout(function(){
-        this.setState({rightSide: right});
-      }.bind(this), 1000);
-      this.setState({archive: false,stream:false,ocean:true})
+  addOcean(i) {
+    if (!this.state.ocean) {
+      setTimeout(
+        function() {
+          this.setState({ rightSide: <div /> });
+        }.bind(this),
+        500
+      );
+      const right = <Ocean key={1} />;
+      setTimeout(
+        function() {
+          this.setState({ rightSide: right });
+        }.bind(this),
+        1000
+      );
+      this.setState({
+        archive: false,
+        stream: false,
+        ocean: true,
+        prompt: false
+      });
     }
   }
 
-  addArchive (i){
-    if (! this.state.archive)
-    {
-      setTimeout(function(){
-        this.setState({rightSide: <div></div>})
-      }.bind(this), 500);
-      const right = <Archive key={2}/>;
-      setTimeout(function(){
-        this.setState({rightSide: right});
-      }.bind(this), 1000);
-      this.setState({archive: true,stream:false,ocean:false})
+  addArchive(i) {
+    if (!this.state.archive) {
+      setTimeout(
+        function() {
+          this.setState({ rightSide: <div /> });
+        }.bind(this),
+        500
+      );
+      const right = <Archive key={2} />;
+      setTimeout(
+        function() {
+          this.setState({ rightSide: right });
+        }.bind(this),
+        1000
+      );
+      this.setState({
+        archive: true,
+        stream: false,
+        ocean: false,
+        prompt: false
+      });
     }
   }
 
-	render(){
-		
-    ;
+  addPrompt(i) {
+    if (!this.state.prompt) {
+      setTimeout(
+        function() {
+          this.setState({ rightSide: <div /> });
+        }.bind(this),
+        500
+      );
+      const right = <Prompt key={3} />;
+      setTimeout(
+        function() {
+          this.setState({ rightSide: right });
+        }.bind(this),
+        1000
+      );
+      this.setState({
+        archive: true,
+        stream: false,
+        ocean: false,
+        prompt: true
+      });
+    }
+  }
 
-		return (
-		<div key={this.state.rightSide}>
+  render() {
+    return (
+      <div key={this.state.rightSide}>
         <CSSTransitionGroup
           transitionName="example"
           transitionAppear={true}
           transitionEnter={true}
-      		transitionLeave={true}>
-      		{this.state.leftSide}
+          transitionLeave={true}
+        >
+          {this.state.leftSide}
           {this.state.rightSide}
         </CSSTransitionGroup>
-      </div>);
-	}
+      </div>
+    );
+  }
 }
 
-export default StreamHolder
+export default StreamHolder;
